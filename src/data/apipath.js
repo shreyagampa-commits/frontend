f56a42d672dae0310c5d4dc14bacec2d3d6346ea
mad674 <144526214+mad674@users.noreply.github.com>
2024-11-01 18:14:52 +0530

    Update apipath.js

--- src/data/apipath.js
+++ src/data/apipath.js
@@ -2,6 +2,6 @@
 // export const API_URL="http://localhost:4000"
 // export const model_URL="http://localhost:5000"
 export const model_URL="https://fastapi-ak4d.onrender.com"
-export const API_URL="https://backend-9oaz.onrender.com/"
+export const API_URL="https://backend-9oaz.onrender.com"
 // export const API_URL="https://goldserver.vercel.app"
 export const clientId="491653773876-drtac3cp61cqpt67d79tjfgnnd1g998n.apps.googleusercontent.com";
